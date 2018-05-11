698fd67bb521214e857ffe3fb65a1ee461e00391
ScriptBoy2000 <31825176+nickolasdaniel@users.noreply.github.com>
2018-05-11 20:17:45 +0000

    Update server.py

--- server.py
+++ server.py
@@ -2,16 +2,18 @@ import socket
 import logging
 import config
 import select
+import struct
 
 class Server(object):
     def __init__(self,setBlocking=False, server_address = None, reuse_port = True, reuse_address = True, port = 8080):
         self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+            
         if server_address:
             self.server_address=("127.0.0.1",port)
         else:
             self.server_address=(socket.gethostbyname(socket.gethostname()),port)
         
+        self.data_struct = struct.Struct("I")
         try:
             self.socket_server.bind(server_address)
         except:
@@ -26,11 +28,37 @@ class Server(object):
         
         self.input = [self.socket_server]
         self.output = []
-        def start_server():
-            self.server_status = True
-            
+        
+        
+    def start_server():
+        self.server_status = True    
             while self.server_status:
                 readable, writeable, exceptional = select.select(input, output, input)
-                
-        
-        
+            
+            for sock in readable:
+                if socket == self.socket_server:
+                    pass            
+                else:
+                    pass
+            for sock in writeable:
+                pass
+            
+            
+            for sock in exceptional:
+                pass
+                    
+    def handle_client(self, client_socket):
+        pass
+    
+    def send_message(self):
+        pass
+    
+   """ def recv_message(self, board_serial):
+        pass
+    
+    def send_signal(self, board_serial):
+        pass
+    """
+    def recv_signal(self):
+        pass
+    
